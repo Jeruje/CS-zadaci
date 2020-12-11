86c630ba612589013732e4f754cdef1ed5a6eb3b
Jeruje <71007799+Jeruje@users.noreply.github.com>
2020-12-11 17:10:12 +0100

    Kap
    Kap koja upada u posudu

---
+++ PrviPismeni/PrviPismeni/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrviPismeni
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+   
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = 500 ;
+            int y = 400;
+            k = new Kap(x, y);
+            
+            Width = 1000;
+            Height = 800;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            timer1.Start();
+        }
+
+        Kap k;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            k.Pomeri(2);
+            Refresh();
+        }
+
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+           
+            Graphics g = CreateGraphics();
+            SolidBrush cetka = new SolidBrush(Color.White);
+            g.FillRectangle(cetka, 0, 0, ClientRectangle.Width, ClientRectangle.Height);
+            
+            k.Crtaj(g);
+            cetka.Color = Color.Purple;
+            g.FillRectangle(cetka, ClientRectangle.Width / 2 - 40, ClientRectangle.Height - 100, 80, 100);
+            g.FillRectangle(cetka, ClientRectangle.Width / 2 - 20, ClientRectangle.Height - 120, 40, 20);
+        }
+    }
+}
---
+++ PrviPismeni/PrviPismeni/Kap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace PrviPismeni
+{
+    class Kap
+    {
+        int x, y;
+        int a=10;
+        
+        public Kap(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+          
+        }
+
+        public void Crtaj(Graphics g)
+        {
+            Pen olovka = new Pen(Color.Aqua, 4);
+            SolidBrush cetkap = new SolidBrush(Color.Aqua);
+            SolidBrush cetkab = new SolidBrush(Color.White);
+            g.DrawEllipse(olovka, x - a, y - a, 2 * a, 2 * a);
+            g.FillEllipse(cetkap, x - a, y - a, 2 * a, 2 * a);
+            g.FillRectangle(cetkab, x - a - 10, y - a, 2 * a + 20, a);
+            Point p1 = new Point(x, y - 2 * a);
+            Point p2 = new Point(x - a, y);
+            Point p3 = new Point(x + a, y);
+            Point[] niz = { p1, p2, p3 };
+            g.FillPolygon(cetkap, niz);
+        }
+
+        public void Pomeri(int dy)
+        {
+            y += dy;
+        }
+
+    }
+}
